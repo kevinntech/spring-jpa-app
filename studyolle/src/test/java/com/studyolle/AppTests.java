18acfcc3b672bc425948de9cf7deb1be26abfce2
kevinntech <kevinntech@nate.com>
2021-02-15 15:25:57 +0900

    섹션 1. 회원 가입 - 프로젝트 만들기

---
+++ studyolle/src/test/java/com/studyolle/AppTests.java
@@ -0,0 +1,13 @@
+package com.studyolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

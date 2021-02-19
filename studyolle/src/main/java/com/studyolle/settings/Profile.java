4567b0f33754d2f708f2de87f2c180b97b8ac91d
kevinntech <kevinntech@nate.com>
2021-02-19 19:34:08 +0900

    섹션 2. 계정 설정 - 프로필 수정 폼

---
+++ studyolle/src/main/java/com/studyolle/settings/Profile.java
@@ -0,0 +1,27 @@
+package com.studyolle.settings;
+
+import com.studyolle.domain.Account;
+import lombok.Data;
+
+@Data
+public class Profile {
+
+    // 짧은 자기소개
+    private String bio;
+
+    // 자신의 블로그, SNS url
+    private String url;
+
+    // 직업
+    private String occupation;
+
+    // 살고 있는 지역
+    private String location;
+
+    public Profile(Account account) {
+        this.bio = account.getBio();
+        this.url = account.getUrl();
+        this.occupation = account.getOccupation();
+        this.location = account.getLocation();
+    }
+}

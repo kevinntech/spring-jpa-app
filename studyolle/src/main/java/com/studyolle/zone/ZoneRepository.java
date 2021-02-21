e9adae06625395f17a4a8d82094ceaa8c231b370
kevinntech <kevinntech@nate.com>
2021-02-21 18:53:37 +0900

    섹션 3. 관심 주제와 지역 정보 - 지역 도메인

---
+++ studyolle/src/main/java/com/studyolle/zone/ZoneRepository.java
@@ -0,0 +1,8 @@
+package com.studyolle.zone;
+
+import com.studyolle.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+    Zone findByCityAndProvince(String cityName, String provinceName);
+}
